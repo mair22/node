5178ed02b9041169d22dccf95afb4de7e67f64b4
mair22 <acediawalker@gmail.com>
2022-08-15 14:06:58 +0700

    test-prettier

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const handlebars = require('express-handlebars');
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
-const route = require('./routes');
+const route = require("./routes");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
-    express.urlencoded({
-        extended: true,
-    }),
+  express.urlencoded({
+    extended: true,
+  })
 );
 
-              app.use(express.json());
+app.use(express.json());
 //XMLHttpRequest, fetch, axious,...
 
 // HTTP logger
@@ -22,15 +22,15 @@ app.use(
 
 //Template engine
 app.engine(
-    'hbs',
-    handlebars.engine({
-        extname: '.hbs',
-    }),
+  "hbs",
+  handlebars.engine({
+    extname: ".hbs",
+  })
 );
-              app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
 
-console.log('PATH: ', path.join(__dirname, 'resources/views'));
+console.log("PATH: ", path.join(__dirname, "resources/views"));
 
 //Home, Search, Contact
 
@@ -39,5 +39,5 @@ console.log('PATH: ', path.join(__dirname, 'resources/views'));
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
