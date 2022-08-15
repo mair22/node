891b621043b7115f0a3024e6cc7be4df5ef8e786
mair22 <acediawalker@gmail.com>
2022-08-15 10:36:28 +0700

    RoutesandControllers

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ const morgan = require("morgan");
 const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
+const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use(express.json());
+//XMLHttpRequest, fetch, axious,...
+
+
 // HTTP logger
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 //Template engine
 app.engine(
@@ -22,22 +31,11 @@ app.set("views", path.join(__dirname, "resources/views"));
 
 console.log("PATH: ", path.join(__dirname, "resources/views"));
 
-//Route
-app.get("/", (req, res) => {
-  res.render("home");
-});
+//Home, Search, Contact
 
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+//Routes init
 
-app.get("/search", (req, res) => {
-  res.render("search");
-});
-
-app.post("/search", (req, res) => {
-  res.send("");
-});
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
