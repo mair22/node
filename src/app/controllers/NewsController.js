d42d4e2b7a9c767420c32a2544f1610add5fa01f
mair22 <acediawalker@gmail.com>
2022-08-15 14:02:45 +0700

    test prettier

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,14 +1,14 @@
 class NewsController {
-    //[GET] / news
-    index(req, res) {
-        res.render('news');
-    }
-    //[GET] /news/:slug
-    show(req, res) {
-        res.send('NEWS DETAIL!!!');
-    }
+  //[GET] / news
+  index(req, res) {
+    res.render('news');
+  }
+  //[GET] /news/:slug
+  show(req, res) {
+    res.send('NEWS DETAIL!!!');
+  }
 }
 
-module.exports = new NewsController;
+module.exports = new NewsController();
 
 const newController = require('./NewsController');
--- src/index.js
+++ src/index.js
@@ -1,42 +1,43 @@
-const express = require("express");
-const path = require("path");
-const morgan = require("morgan");
-const handlebars = require("express-handlebars");
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 
-app.use(express.json());
+              app.use(express.json());
 //XMLHttpRequest, fetch, axious,...
-
 
 // HTTP logger
 // app.use(morgan("combined"));
 
 //Template engine
 app.engine(
-  "hbs",
-  handlebars.engine({
-    extname: ".hbs",
-  })
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+              app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
-console.log("PATH: ", path.join(__dirname, "resources/views"));
+console.log('PATH: ', path.join(__dirname, 'resources/views'));
 
 //Home, Search, Contact
 
 //Routes init
 
-route(app)
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
